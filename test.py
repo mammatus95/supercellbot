5bf2911ea26e71f9c81d435307149ef1986844a2
MK <mortenkretschmer@yahoo.de>
2018-05-04 06:54:10 +0200

    Create test.py

---
+++ test.py
@@ -0,0 +1,23 @@
+from twython import Twython
+"""
+consumer_key = 'your_consumer_key'
+consumer_secret = 'your_consumer_secret'
+access_token = 'your_access_token'
+access_token_secret = 'your_access_token_secret'
+"""
+
+from auth import (
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+twitter = Twython(
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+message = "Hello world!"
+twitter.update_status(status=message)
+print("Tweeted: {}".format(message))
